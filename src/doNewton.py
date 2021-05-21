7da3c2a2f4be1806007e214d2b9b859fa12ad008
Edson Cizeski <cizeskiedson@gmail.com>
2021-05-21 16:17:19 -0300

    work done

--- src/doNewton.py
+++ src/doNewton.py
@@ -6,6 +6,7 @@ import raiz
 import math
 
 listaX = []
+listaInv = []
 
 def ieee(x: float)->float:
     '''
@@ -40,6 +41,21 @@ def nr(x:float, f:float) -> None:
         xk = 1/2 * (listaX[k-1] + ((1+f)/listaX[k-1]))
         listaX.append(xk)
 
+def nr_inverso(x:float, f:float) -> None:
+    '''
+    Aplica Newton-Rapson em 1/X
+    '''
+    x0 = chute(f)
+    x0 = 1/x0
+    for k in range(0, 6):
+        if(k == 0):
+            listaInv.append(x0)
+            continue
+        xk = 1/2 * listaInv[k-1] * (3 - ((1+f)*(pow(listaInv[k-1], 2))))
+        listaInv.append(xk)
+
+
+
 def raizQuad(value:float, e:int) -> float:
     '''
     Retorna o resultado final
@@ -47,6 +63,15 @@ def raizQuad(value:float, e:int) -> float:
     if e%2: #impar
         return (pow(2, (e-1)/2) * (math.sqrt(2) * value))
     else: return (pow(2, e/2) * value)
+
+
+def raizQuadInv(value:float, e:int) -> float:
+    '''
+    Retorna o resultado final da inversa
+    '''
+    if e%2: #impar
+        return value/(pow(2, (e-1)/2) * math.sqrt(2))
+    else: return value/(pow(2, e/2))
 
 
 
@@ -67,4 +92,11 @@ def main():
         print(f"{i} - resultado {math.sqrt(x) - raizQuad(listaX[i], e)}")
         print("\n\n\n")
 
+    nr_inverso(x, f)
+    print(f"lista inv {listaInv}")
+
+    for i in range(6):
+        print(f"{math.sqrt(x)} - {raizQuad(listaInv[i], e)}")
+        print(f"{i} - resultado {1/math.sqrt(x) - raizQuadInv(listaInv[i], e)}")
+        print("\n\n\n")
 main()
